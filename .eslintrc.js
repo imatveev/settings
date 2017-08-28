e8b6785c1a80b789e7d7194b1ea562729abd71b2
Ivan Matvieiev <ivan/m@walkme.com>
2017-08-28 09:12:32 +0300

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,40 @@
 'use strict';
+
 module.exports = {
     'rules': {
-        'comma-spacing': [1, {
+        'comma-spacing': [ 1, {
             'before': false,
             'after':  true
-        }],
-        'no-dupe-args':         2,
-        'no-dupe-keys':         2,
-        'no-extra-semi':        1,
-        'use-isnan':            1,
-	'vars-on-top':          1,
-	'no-loop-func':         1,
-	'no-loop-func':         1,
-        'no-console':           1,
-        'no-unreachable':       2,
-        'no-native-reassign':   2,
-        'no-extra-bind':        2,
-        'guard-for-in':         2,
-        'no-var':               2,
-        'prefer-const':         0,
-	'no-multi-spaces':      0,
-	'arrow-parens':         0,
-	'arrow-body-style':     0,
-	'key-spacing':          0,
-	'array-bracket-spacing': 0,
-	'import/no-extraneous-dependencies': ["error", {"devDependencies": ["**/test/**"]}],
-	'no-param-reassign':    [2, { "props": true, "ignorePropertyModificationsFor": ["app", "async"] }],
-        'no-extra-parens':      [1, 'all'],
-        'strict':               [1, 'global'],
-        'comma-dangle':         [0, 'always-multiline'],
-        'indent':               [1, 4, {'MemberExpression': 'off'}],
-        'quotes':               [0, 'single'],
-        'linebreak-style':      [2, 'unix'],
-        'semi':                 [1, 'always'],
-        'no-unused-vars':       [1, {'vars': 'all', 'args': 'after-used'}]
+        } ],
+        'no-dupe-args':                      2,
+        'no-dupe-keys':                      2,
+        'no-extra-semi':                     1,
+        'use-isnan':                         1,
+    	'vars-on-top':                       1,
+    	'no-loop-func':                      1,
+    	'no-loop-func':                      1,
+        'no-console':                        1,
+        'no-unreachable':                    2,
+        'no-native-reassign':                2,
+        'no-extra-bind':                     2,
+        'guard-for-in':                      2,
+        'no-var':                            2,
+        'prefer-const':                      0,
+    	'no-multi-spaces':                   0,
+    	'arrow-parens':                      0,
+    	'arrow-body-style':                  0,
+    	'key-spacing':                       0,
+    	'array-bracket-spacing':             0,
+    	'import/no-extraneous-dependencies': [ "error", {"devDependencies": ["**/test/**"]} ],
+    	'no-param-reassign':                 [ 2, { "props": true, "ignorePropertyModificationsFor": ["app", "async"] } ],
+        'no-extra-parens':                   [ 1, 'all' ],
+        'strict':                            [ 1, 'global' ],
+        'comma-dangle':                      [ 0, 'always-multiline' ],
+        'indent':                            [ 1, 4, {'MemberExpression': 'off'} ],
+        'quotes':                            [ 0, 'single' ],
+        'linebreak-style':                   [ 2, 'unix' ],
+        'semi':                              [ 1, 'always' ],
+        'no-unused-vars':                    [ 1, {'vars': 'all', 'args': 'after-used'} ]
     },
     'parserOptions': {
         'ecmaVersion': 2017,
@@ -47,7 +48,7 @@ module.exports = {
         'mocha':     true,
         'es6':       true,
         'phantomjs': true,
-	'browser':   true
+	    'browser':   true
     },
-    'extends': ['eslint:recommended', 'airbnb-base']
+    'extends': [ 'eslint:recommended', 'airbnb-base' ]
 };

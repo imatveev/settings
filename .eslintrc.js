49972c8d9dc8e1bc4d6feeb26ebc10675bed5fe2
Ivan <prodesigning@ya.ru>
2017-08-28 18:04:16 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,21 +12,20 @@ module.exports = {
         'use-isnan':                         1,
     	'vars-on-top':                       1,
     	'no-loop-func':                      1,
-    	'no-loop-func':                      1,
         'no-console':                        1,
         'no-unreachable':                    2,
         'no-native-reassign':                2,
         'no-extra-bind':                     2,
         'guard-for-in':                      2,
         'no-var':                            2,
-        'prefer-const':                      0,
+        'prefer-const':                      2,
     	'no-multi-spaces':                   0,
     	'arrow-parens':                      0,
     	'arrow-body-style':                  0,
     	'key-spacing':                       0,
     	'array-bracket-spacing':             0,
-    	'import/no-extraneous-dependencies': [ "error", {"devDependencies": ["**/test/**"]} ],
-    	'no-param-reassign':                 [ 2, { "props": true, "ignorePropertyModificationsFor": ["app", "async", "ctx", "object"] } ],
+    	'import/no-extraneous-dependencies': [ 'error', {'devDependencies': ['**/test/**']} ],
+    	'no-param-reassign':                 [ 2, { 'props': true, 'ignorePropertyModificationsFor': ['app', 'async', 'ctx', 'object'] } ],
         'no-extra-parens':                   [ 1, 'all' ],
         'strict':                            [ 1, 'global' ],
         'comma-dangle':                      [ 0, 'always-multiline' ],
@@ -34,7 +33,13 @@ module.exports = {
         'quotes':                            [ 0, 'single' ],
         'linebreak-style':                   [ 2, 'unix' ],
         'semi':                              [ 1, 'always' ],
-        'no-unused-vars':                    [ 1, {'vars': 'all', 'args': 'after-used'} ]
+        'no-unused-vars':                    [ 1, {'vars': 'all', 'args': 'after-used', 'varsIgnorePattern': '^h$' } ],
+        'jsx-a11y/href-no-hash':             0,
+        'import/no-unresolved':              0,
+        'import/extensions':                 0,
+        'react/jsx-indent':                  [ 1, 4 ],
+        'react/jsx-filename-extension':      0,
+        'react/react-in-jsx-scope':          0
     },
     'parserOptions': {
         'ecmaVersion': 2017,
@@ -50,5 +55,10 @@ module.exports = {
         'phantomjs': true,
 	    'browser':   true
     },
-    'extends': [ 'eslint:recommended', 'airbnb-base' ]
+    'extends': [ 'eslint:recommended', 'airbnb' ],
+    'settings': {
+        'import/resolver': {
+            'webpack': {}
+        }
+    }
 };
